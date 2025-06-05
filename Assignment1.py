f40c3abdb63b3fe1bef7a223cce3731e647327ac
vaibhavgaur <110227490+vaibhav4gaur@users.noreply.github.com>
2025-06-05 23:10:55 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number: "))
+
+print("Addition :",a+b)
+print("Substraction :",a-b)
+print("Multiplication :",a*b)
+if b!=0:
+    print("Divison :",a/b)
+else:
+    print("Divison by zero is not possible")
+
+    
