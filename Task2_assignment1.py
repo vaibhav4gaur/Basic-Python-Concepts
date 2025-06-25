87f3e1dd8b259906ea903073db05be9619c3c49e
vaibhavgaur <110227490+vaibhav4gaur@users.noreply.github.com>
2025-06-25 10:30:31 +0530

    Update and rename Task2_assignment1.py to Update_assignment1.py

--- Task2_assignment1.py
+++
@@ -1,8 +0,0 @@
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-
-full_name = first_name+ " " +last_name
-
-print(f"Hello, {full_name}! Welcome to the Python program.")
-
-
---
+++ Update_assignment1.py
@@ -0,0 +1,6 @@
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+
+full_name = fname+ " " +lname
+
+print(f"Hello, {full_name}! Welcome to the Python program.")
